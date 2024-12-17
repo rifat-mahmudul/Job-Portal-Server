e41b717605123731e654f62d12b514e4615c6d14
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-12-17 11:29:33 +0600

    get category wise data

--- index.js
+++ index.js
@@ -81,7 +81,10 @@ async function run() {
     })
 
     app.get('/rooms', async (req, res) => {
-      const result = await roomsCollection.find().toArray();
+      const category = req.query.category;
+      let query = {};
+      if(category && category !== 'null') query = {category}
+      const result = await roomsCollection.find(query).toArray();
       res.send(result)
     })
 
