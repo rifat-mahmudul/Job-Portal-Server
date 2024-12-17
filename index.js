fa8d2734026a1f2d913d6b837a61b588d261eea5
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-12-17 10:02:13 +0600

    get single data

--- index.js
+++ index.js
@@ -85,6 +85,13 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/rooms/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await roomsCollection.findOne(query);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
     console.log(
