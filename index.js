068c250489adc3d4bc8e0afefd024704276cf678
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-12-19 15:22:50 +0600

    get a user data by email

--- index.js
+++ index.js
@@ -121,6 +121,13 @@ async function run() {
       res.send(result);
     })
     
+    //get a user by email
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = {email : email};
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+    })
 
   
     //post room on DB
